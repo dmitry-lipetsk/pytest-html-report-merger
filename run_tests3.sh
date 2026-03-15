7e3e54eeedcb827d09cbc6997c3a0cdef3e20d3a
d.kovalenko <d.kovalenko@postgrespro.ru>
2026-03-15 14:53:56 +0300

    CI: an explicit test pf pytest-html==4.0.2 is added

--- run_tests3.sh
+++ run_tests3.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z ${PYTEST_HTML_SPEC+x} ]; then
+  echo "ERROR: PYTEST_HTML_SPEC is not defined."
+  exit 1
+fi
+
 set -eux
 
 # prepare python environment
@@ -8,7 +13,13 @@ rm -rf $VENV_PATH
 python -m venv "${VENV_PATH}"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source "${VENV_PATH}/bin/activate"
-pip install -r tests/requirements.txt
+pip install bs4 pytest pytest-xdist pytest-rerunfailures
+
+if [ "$PYTEST_HTML_SPEC" = "default" ]; then
+  pip install "pytest-html>=4.0.2"
+else
+  pip install "pytest-html${PYTEST_HTML_SPEC}"
+fi
 
 # run builtin tests
 python3 -m pytest -l -vvv -n 4 tests
